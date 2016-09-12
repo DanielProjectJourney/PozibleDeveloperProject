d9da9e0f3b9d32a26740bd2b0d37416cc03be12c
daniel zhang <daniel@192-168-1-117.tpgi.com.au>
2016-09-12 15:29:53 +1000

    Add Router Function

--- client/templates/targets/target_list.js
+++ client/templates/targets/target_list.js
@@ -1,32 +1,7 @@
-var targetsData = [
-  {
-    Name: 'Introducing Telescope',
-    Type: 'Individual Project',
-    StartDate: '10-10-2016',
-    EndDate: '10-10-2017',
-    TotalPercentage: '80%',
-    SeasonId: "3",
-    UserIds: "26299038",
-  },
-  {
-    Name: 'Alipay',
-    Type: 'Company-wide',
-    StartDate: '10-10-2016',
-    EndDate: '10-10-2017',
-    TotalPercentage: '80%',
-    SeasonId: "3",
-    UserIds: "26299039",
-  },
-  {
-    Name: 'Google Tensonflow',
-    Type: 'Compnay-wide',
-    StartDate: '10-10-2016',
-    EndDate: '10-10-2017',
-    TotalPercentage: '80%',
-    SeasonId: "3",
-    UserIds: "26299040",
-  }
-];
+
+
 Template.targetsList.helpers({
-  targets: targetsData
+  targets: function() {
+    return Targets.find()
+  }
 });
---
+++ server/seeds.js
@@ -0,0 +1,21 @@
+if (Targets.find().count() === 0) {
+  Targets.insert({
+    Name: 'Pozible FinTech App',
+    Type: 'Individual Project',
+    StartDate: '10-10-2016',
+    EndDate: '10-10-2017',
+    TotalPercentage: '80%',
+    SeasonId: "3",
+    UserIds: "26299038",
+  });
+
+  Targets.insert({
+     Name: 'Alipay',
+     Type: 'Company-wide',
+     StartDate: '10-10-2016',
+     EndDate: '10-10-2017',
+     TotalPercentage: '50%',
+     SeasonId: "3",
+     UserIds: "26299039",
+  })
+}
